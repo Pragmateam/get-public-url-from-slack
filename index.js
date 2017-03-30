aa0045b98431da273a57c8c2e741fb805dac7d4a
Alabê Duarte <alabeduarte@gmail.com>
2017-03-30 11:08:40 +1100

    Update index to be aws lambda compliant

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const getPublicUrl = require('./src/getPublicUrl');
 
-const privateUrl = process.argv.slice(2)[0];
-const token = process.env.TOKEN;
+exports.handler = (event, context, callback) => {
+  const privateUrl = event.privateUrl;
+  const token = process.env.TOKEN;
 
-if (!token) throw new Error('Variable TOKEN is not set');
+  if (!token) throw new Error('Variable TOKEN is not set');
 
-getPublicUrl(privateUrl, token)
-  .then(response => console.info(response))
-  .catch(err => console.error(err));
+  getPublicUrl(privateUrl, token)
+      .then(response => callback(null, response))
+      .catch(err => console.error(err));
+};
