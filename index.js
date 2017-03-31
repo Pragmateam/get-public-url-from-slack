7958722602b22b1872fd166598bec60dcc65422d
Alabê Duarte <alabeduarte@gmail.com>
2017-03-31 12:01:00 +1100

    Throw error if SLACK_API_TOKEN is missing before anything else

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ exports.handler = (event, context, callback) => {
   const privateUrl = event.url;
   const slackAPIToken = process.env.SLACK_API_TOKEN;
 
+  if (!slackAPIToken) throw new Error('Missing Slack API token');
+
   console.info('privateUrl: ', privateUrl);
   console.info('slack API token: ', slackAPIToken.replace(/[^-]/g, '*'));
-
-  if (!slackAPIToken) throw new Error('Missing Slack API token');
 
   getPublicUrl(privateUrl, slackAPIToken)
       .then(response => callback(null, response))
