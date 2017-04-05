2691517f238aae057193bba28a8562f46e5076e0
Alabê Duarte <alabeduarte@gmail.com>
2017-04-05 14:34:30 +1000

    Add logs for getPublicUrl response

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const getPublicUrl = require('./src/getPublicUrl');
 
 exports.handler = (event, context, callback) => {
+  console.info('event: ', event);
+
   const privateUrl = event.url;
   const slackAPIToken = process.env.SLACK_API_TOKEN;
 
   if (!slackAPIToken) throw new Error('Missing Slack API token');
 
-  console.info('privateUrl: ', privateUrl);
   console.info('slack API token: ', slackAPIToken.replace(/[^-]/g, '*'));
 
   getPublicUrl(privateUrl, slackAPIToken)
-      .then(response => callback(null, response))
-      .catch(err => console.error(err));
+    .then(response => {
+      console.log('response: ', response);
+
+      return callback(null, response);
+    }).catch(err => console.error(err));
 };
